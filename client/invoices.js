3dbaa097f58a36e56838f553c6a0838cce063775
Michael de Wal <=>
2016-04-04 15:23:40 +0900

    Added infinite scrolling functionality to the main functionality. It will scroll when you reach the bottom.

--- client/invoices.js
+++ client/invoices.js
@@ -1,17 +1,15 @@
 Router.setRoute('invoices');
-const LIMIT_INCREASE = 50;
 
 Template.registerHelper('formatDate', function(date) {
     return moment(date).format('DD-MM-YYYY');
 });
 
 Template.invoicesTemplate.helpers({
-    invoiceData: function() {
-
+    invoiceData: () => {
         var react = Router.reactvar.get();
         return invoices.byTimeRange(Router.getParams().type, Router.getQueryParams());
     },
-    direction: function(params) {
+    direction: (params) => {
         if(params === undefined)
             return '';
         else if(params === 'ASC')
@@ -19,6 +17,10 @@ Template.invoicesTemplate.helpers({
         else
             return 'down';
 
+    },
+    moreResults: () => {
+        var react = Router.reactvar.get();
+        return !(invoices.find().count() < parseInt(Router.getQueryParams().limit));
     }
 });
 
@@ -52,5 +54,26 @@ Template.invoicesTemplate.events({
     }
 });
 
+function showMoreResults() {
+    var threshold, target = $("#showMoreResults");
+    if (!target.length) return;
+
+    threshold = $(window).scrollTop() + $(window).height() - target.height();
+    if (target.offset().top <= threshold) {
+        if (!target.data("visible")) {
+            // console.log("target became visible (inside viewable area)");
+            target.data("visible", true);
+            // Either add the increased by limit to the current limit inside the URL or when no limit is set in the URL do the limit times 2
+            Router.addQueryParam('limit',(Router.getQueryParams().limit !== undefined)?parseInt(Router.getQueryParams().limit)+invoices.limit_increase_by:invoices.limit_increase_by*2);
+            Router.go('');
+        }
+    } else {
+        if (target.data("visible")) {
+            // console.log("target became invisible (below viewable arae)");
+            target.data("visible", false);
+        }
+    }
+}
+
 // Check for scrolling
-$(window).scroll();
+$(window).scroll(showMoreResults);
